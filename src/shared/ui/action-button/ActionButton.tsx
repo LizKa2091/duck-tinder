c7fdc2c7699bcd6b1a6edff4c4c7a90bc9be5f34
Liza <lizka4231@gmail.com>
2025-12-19 14:22:17 +0300

    feat(action-button): add unnecessary disabled prop

--- src/shared/ui/action-button/ActionButton.tsx
+++ src/shared/ui/action-button/ActionButton.tsx
@@ -6,20 +6,23 @@ import styles from './ActionButton.module.scss';
 interface IActionButtonProps {
    text?: string;
    type?: 'match' | 'message';
+   disabled?: boolean;
    onClick: () => void;
 }
 
 
-export const ActionButton: FC<IActionButtonProps> = ({ text, type, onClick }) => {
+export const ActionButton: FC<IActionButtonProps> = ({ text, type, disabled, onClick }) => {
    const buttonTitle = type === 'match' ? 'Мэтч' : type === 'message' ? 'Написать сообщение' : '';
 
    return (
       <button 
          onClick={onClick} 
          title={buttonTitle}
+         disabled={disabled}
          className={clsx(
             styles.button, 
             type === 'match' && styles.match, 
+            disabled && styles.disabled,
             type === 'message' && styles.message
          )} 
       >
